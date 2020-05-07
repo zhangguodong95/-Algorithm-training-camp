c08bcfb3fa767364110c1f3e70bc968d151117cd
zhangguodong <2226007790@qq.com>
2020-05-07 11:05:55 +0800

    添加dfs模板

--- 第九课 深度优先搜索与广度优先搜索/code/src/Template.java
+++ 第九课 深度优先搜索与广度优先搜索/code/src/Template.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 /**
  * @author zgd
  * @version 1.0
@@ -5,5 +7,26 @@
  */
 
 public class Template {
-    //DFS
+    //dfs
+
+    HashSet<Node> visited = new HashSet<>();
+
+    public void dfs(Node node, HashSet<Node> visited) {
+        //terminator
+        if (visited.contains(node)) {
+            //already visited
+            return;
+        }
+
+        visited.add(node);
+
+        // process current node here
+
+        if (visited.contains(node.left)) {
+            dfs(node.left,visited);
+        }
+        if (visited.contains(node.right)) {
+            dfs(node.right,visited);
+        }
+    }
 }
